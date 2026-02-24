26307ac070322c08c69ce61844805557f062763b
GooberCoder <vaino.o.hanninen@gmail.com>
2026-02-24 18:17:44 +0200

    ello

---
+++ MiniGames/Assets/scripts/imageshadethingcontroller.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class imageshadethingcontroller : MonoBehaviour
+{
+    public float fadetime = 1f;
+    private Image foto;
+
+    private Color whentimebegantherewasthecubecolor;
+
+    void Awake()
+    {
+        foto = GetComponent<Image>();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
