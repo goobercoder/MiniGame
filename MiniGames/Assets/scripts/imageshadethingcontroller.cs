a617ba5a2d1e67d5fbb3fbb3bc06624d301800b2
GooberCoder <vaino.o.hanninen@gmail.com>
2026-03-03 18:12:45 +0200

    fukkin bores

--- MiniGames/Assets/scripts/imageshadethingcontroller.cs
+++ MiniGames/Assets/scripts/imageshadethingcontroller.cs
@@ -1,16 +1,54 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class imageshadethingcontroller : MonoBehaviour
 {
     public float fadetime = 1f;
     private Image foto;
 
-    private Color whentimebegantherewasthecubecolor;
+    private Color whentimebegantherewasthecolor;
+
+    private Coroutine coroutine;
 
     void Awake()
     {
         foto = GetComponent<Image>();
+        if (foto == null)
+        {
+            Debug.Log("foto went boom");
+            return;
+        }
+        whentimebegantherewasthecolor = foto.color;
+    }
+    private IEnumerator FadeAschtonHall(float TargetAlphaomgfuckinalfasocrazyy)
+    {
+        float seconds = 0f;
+        float startAlpha = foto.color.a;
+
+        while (seconds < fadetime)
+        {
+            seconds += Time.deltaTime;
+
+            Color retardedColor = foto.color;
+            float t = seconds / fadetime;
+            retardedColor.a = Mathf.Lerp(startAlpha, TargetAlphaomgfuckinalfasocrazyy, t);
+            foto.color = retardedColor;
+
+            yield return null;
+        }
+        Color finalcountdowndododododododododoooooodooooooo = foto.color;
+        finalcountdowndododododododododoooooodooooooo.a = TargetAlphaomgfuckinalfasocrazyy;
+        foto.color = finalcountdowndododododododododoooooodooooooo;
+    }
+
+    private void Startfade(float TargetAlphaomgfuckinalfasocrazyy)
+    {
+        if(coroutine == null)
+        {
+            StopCoroutine(FadeAschtonHall(TargetAlphaomgfuckinalfasocrazyy));
+        }
+        coroutine = StartCoroutine(FadeAschtonHall(TargetAlphaomgfuckinalfasocrazyy));
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
