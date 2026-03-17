9ff5a337adf03e43e8ff51c5a0fafb3e6f71b2d7
GooberCoder <vaino.o.hanninen@gmail.com>
2026-03-17 18:16:23 +0200

    heheidk

--- MiniGames/Assets/scripts/RotationEffect.cs
+++ MiniGames/Assets/scripts/RotationEffect.cs
@@ -2,14 +2,17 @@ using UnityEngine;
 
 public class RotationEffect : MonoBehaviour
 {
-    public float meatMult = 5f;
+    public float meatMultMult = 15f;
+    private float meatMult = 1f;
     public float angleMult = 10f;
+    
     void Update()
     {
         float meat = Mathf.Sin(Time.time) * meatMult;
         float angle = Mathf.Sin(Time.time) * angleMult;
         float tilt = Mathf.Sin(Time.time * meat) * angle;
         gameObject.transform.localRotation = Quaternion.Euler(0, 0, tilt);
+        meatMult = Random.Range(0f, 1f) * meatMultMult;
         
     }
 }
--- MiniGames/Assets/scripts/sector.cs
+++ MiniGames/Assets/scripts/sector.cs
@@ -3,18 +3,42 @@ using TMPro;
 
 public class sector : MonoBehaviour
 {
-    public Color normalCocklor = Color.white;
+    public Color normalCocklor = new Color(118, 134, 0);
     public Color erectCocklor = Color.purple;
     public TMP_Text[] consumptionary_button_products;
     private int andNowWereOoon = 0;
+    
     void Start()
     {
-        
+        ChangeColoursUhImBriish();
     }
 
     
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            andNowWereOoon = (andNowWereOoon -1 + consumptionary_button_products.Length) % consumptionary_button_products.Length;
+            ChangeColoursUhImBriish();
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            andNowWereOoon = (andNowWereOoon +1) % consumptionary_button_products.Length;
+            ChangeColoursUhImBriish();
+        }
+    }
+    void ChangeColoursUhImBriish()
+    {
+        for(int i = 0; i < consumptionary_button_products.Length; i++) 
+        {
+            if(i == andNowWereOoon)
+            {
+                consumptionary_button_products[i].color = erectCocklor;
+            }
+            else
+            {
+                consumptionary_button_products[i].color = normalCocklor;
+            }
+        }
     }
 }
